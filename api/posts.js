3174fe10d1b1aac22481873cd24ce250a580cd3c
Micpeace <Makubexku@gmail.com>
2026-03-04 22:20:36 +0800

    Update posts.js

--- api/posts.js
+++ api/posts.js
@@ -1,4 +1,12 @@
 export default async function handler(req, res) {
+  // CORS
+res.setHeader("Access-Control-Allow-Origin", "*")
+res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS")
+res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+if (req.method === "OPTIONS") {
+  return res.status(200).end()
+}
   try {
     const notionToken = process.env.NOTION_TOKEN;
     const databaseId = process.env.NOTION_DATABASE_ID;
